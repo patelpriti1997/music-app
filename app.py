6612538e22f4283ba055f0deecce20131d255426
patelpriti1997 <patelpriti3041997@gmail.com>
2026-02-07 00:28:08 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, send_file
+import requests, time, os
+
+app = Flask(__name__)
+
+HF_TOKEN = "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"
+
+@app.route("/generate", methods=["POST"])
+def generate():
+
+    prompt = request.form.get("prompt")
+
+    headers = {
+        "Authorization": f"Bearer {HF_TOKEN}"
+    }
+
+    response = requests.post(
+        "https://api-inference.huggingface.co/models/facebook/musicgen-small",
+        headers=headers,
+        json={"inputs": prompt}
+    )
+
+    filename = f"music_{int(time.time())}.wav"
+
+    with open(filename, "wb") as f:
+        f.write(response.content)
+
+    return send_file(filename, as_attachment=True)
+
+
+app.run(host="0.0.0.0", port=10000)
